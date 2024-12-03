d4bca32e2db1246e8a23cb5291b72119a55a38da
Yasura Lakshitha <lakshitha200087@gmail.com>
2024-12-03 18:36:20 +0530

    configured cli

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Application initialized')
+import * as readLine from "node:readline";
+import {Interface} from "node:readline";
+
+const cli: Interface = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+cli.question("caching-proxy ", (userInput: string) => {
+    console.log(userInput)
+})
